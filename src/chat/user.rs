25ceb847614d2ab28c276886c8e9d1cc3a235f5b
Andrew Wason <rectalogic@rectalogic.com>
2026-03-16 17:37:09 -0400

    consolidate ChatUser component

--- src/chat/user.rs
+++ src/chat/user.rs
@@ -1,79 +1,79 @@
-use bevy::prelude::*;
 use bevy::{
     prelude::*,
     tasks::{IoTaskPool, Task, futures_lite::future},
 };
 use iroh::{Endpoint, RelayMode, SecretKey, protocol::Router};
 use iroh_gossip::{Gossip, net::GOSSIP_ALPN};
-use std::ops::DerefMut;
 
 pub fn plugin(app: &mut App) {
-    app.add_observer(user_loader_added)
-        .add_systems(Update, poll_loader_task);
+    app.add_observer(user_added)
+        .add_systems(Update, poll_loading);
 }
 
 #[derive(Component)]
-pub struct ChatUserLoader {
-    secret_key: SecretKey,
-    task: Option<Task<Result<ChatUser, BevyError>>>,
-}
+pub struct ChatUser(UserState);
 
-impl ChatUserLoader {
-    pub fn new(secret_key: SecretKey) -> Self {
-        Self {
-            secret_key,
-            task: None,
-        }
-    }
-}
-
-#[derive(Component)]
-pub struct ChatUser {
-    endpoint: Endpoint,
-    router: Router,
-    gossip: Gossip,
+enum UserState {
+    Loading {
+        secret_key: SecretKey,
+        task: Option<Task<Result<UserState, BevyError>>>,
+    },
+    Loaded {
+        endpoint: Endpoint,
+        router: Router,
+        gossip: Gossip,
+    },
 }
 
 impl ChatUser {
-    async fn new(secret_key: SecretKey) -> Result<Self, BevyError> {
-        let endpoint = Endpoint::builder()
-            .secret_key(secret_key)
-            .relay_mode(RelayMode::Default)
-            .bind()
-            .await?;
-        let gossip = Gossip::builder().spawn(endpoint.clone());
-        // Wait for home relay
-        endpoint.online().await;
-        let router = Router::builder(endpoint.clone())
-            .accept(GOSSIP_ALPN, gossip.clone())
-            .spawn();
-        Ok(Self {
-            endpoint,
-            router,
-            gossip,
+    pub fn new(secret_key: SecretKey) -> Self {
+        Self(UserState::Loading {
+            secret_key,
+            task: None,
         })
     }
 }
 
-fn user_loader_added(user_loader: On<Add, ChatUserLoader>, mut query: Query<&mut ChatUserLoader>) {
-    if let Ok(mut loader) = query.get_mut(user_loader.entity) {
-        let secret_key = loader.secret_key.clone();
-        loader.task = Some(IoTaskPool::get().spawn(async move { ChatUser::new(secret_key).await }));
+async fn load_user(secret_key: SecretKey) -> Result<UserState, BevyError> {
+    let endpoint = Endpoint::builder()
+        .secret_key(secret_key)
+        .relay_mode(RelayMode::Default)
+        .bind()
+        .await?;
+    let gossip = Gossip::builder().spawn(endpoint.clone());
+    // Wait for home relay
+    endpoint.online().await;
+    let router = Router::builder(endpoint.clone())
+        .accept(GOSSIP_ALPN, gossip.clone())
+        .spawn();
+    Ok(UserState::Loaded {
+        endpoint,
+        router,
+        gossip,
+    })
+}
+
+fn user_added(chat_user: On<Add, ChatUser>, mut query: Query<&mut ChatUser>) {
+    if let Ok(mut chat_user) = query.get_mut(chat_user.entity)
+        && let UserState::Loading {
+            ref secret_key,
+            ref mut task,
+        } = chat_user.0
+    {
+        let secret_key = secret_key.clone();
+        *task = Some(IoTaskPool::get().spawn(async move { load_user(secret_key).await }));
     }
 }
 
-fn poll_loader_task(mut commands: Commands, mut loader: Single<(Entity, &mut ChatUserLoader)>) {
-    let (entity, loader) = loader.deref_mut();
-    if let Some(ref mut task) = loader.task
+fn poll_loading(mut chat_user: Single<&mut ChatUser>) {
+    if let UserState::Loading {
+        task: Some(ref mut task),
+        ..
+    } = chat_user.0
         && let Some(result) = future::block_on(future::poll_once(task))
     {
         match result {
-            Ok(user) => {
-                commands
-                    .entity(*entity)
-                    .insert(user)
-                    .remove::<ChatUserLoader>();
-            }
+            Ok(user_state) => chat_user.0 = user_state,
             Err(e) => error!("Failed to initialize chat user: {e:?}"),
         }
     }
