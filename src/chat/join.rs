546a3d85454910c34cb5b38c6662e4e0c53194fe
Andrew Wason <rectalogic@rectalogic.com>
2026-03-19 17:00:18 -0400

    ignore CAS when publishing. move publishing loop into RoomTopic

--- src/chat/join.rs
+++ src/chat/join.rs
@@ -1,12 +1,10 @@
-use std::time::Duration;
-
 use bevy::{
     prelude::*,
     tasks::futures_lite::{self, stream},
 };
-use iroh::{EndpointId, SecretKey, endpoint_info::EndpointIdExt};
+use iroh::SecretKey;
 use iroh_gossip::Gossip;
-use pkarr::{Client, dns::rdata::RData};
+use pkarr::Client;
 use tokio_stream::StreamExt;
 
 use crate::{
@@ -95,7 +93,10 @@ async fn room_event_loop(
         .split();
 
     let _endpoint_publisher_task = Task::new(tokio::task::spawn(async move {
-        if let Err(e) = publish_endpoint_cname(client, topic, secret_key.public(), 15).await {
+        if let Err(e) = topic
+            .publish_endpoint_cname(&client, secret_key.public(), 15)
+            .await
+        {
             error!("Endpoint CNAME publisher failed: {e:?}")
         }
     }));
@@ -129,58 +130,6 @@ async fn room_event_loop(
     }
 
     Ok(())
-}
-
-// Periodically publish our EndpointId to the rooms DNS CNAME
-async fn publish_endpoint_cname(
-    client: Client,
-    topic: RoomTopic,
-    endpoint_id: EndpointId,
-    ttl: u32,
-) -> Result<(), BevyError> {
-    let delay = Duration::from_secs((ttl as f64 * 0.75) as u64);
-    let endpoint_z32 = endpoint_id.to_z32();
-    let endpoint_name = pkarr::dns::Name::new(&endpoint_z32)?;
-
-    loop {
-        let mut builder = pkarr::SignedPacket::builder();
-        let (builder, cas) = if let Some(most_recent) = client
-            .resolve_most_recent(&topic.dns_publisher_keypair().public_key())
-            .await
-        {
-            for record in most_recent.fresh_resource_records(topic.bootstrap_dns_name()) {
-                match record.rdata {
-                    RData::CNAME(ref cname) if cname.0 != endpoint_name => {
-                        match record.ttl.overflowing_sub(most_recent.elapsed()) {
-                            (_, true) => {}
-                            (ttl, false) => {
-                                let mut record = record.clone();
-                                record.ttl = ttl;
-                                builder = builder.record(record);
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-            }
-            (builder, Some(most_recent.timestamp()))
-        } else {
-            (builder, None)
-        };
-
-        let signed_packet = builder
-            .cname(
-                topic.bootstrap_dns_name().try_into()?,
-                endpoint_name.clone(),
-                ttl,
-            )
-            .sign(topic.dns_publisher_keypair())?;
-
-        if let Err(e) = client.publish(&signed_packet, cas).await {
-            warn!("Failed to publish CNAME: {e:?}");
-        }
-        tokio::time::sleep(delay).await;
-    }
 }
 
 fn handle_gossip_event(mut commands: Commands, room_connections: Query<(Entity, &RoomConnection)>) {
--- src/chat/room.rs
+++ src/chat/room.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use bevy::prelude::*;
 use iroh::{EndpointId, endpoint_info::EndpointIdExt};
 use iroh_gossip::TopicId;
@@ -86,5 +88,56 @@ impl RoomTopic {
         }
 
         bootstrap_endpoint_ids
+    }
+
+    // Periodically publish our EndpointId to the rooms DNS CNAME
+    pub async fn publish_endpoint_cname(
+        &self,
+        client: &pkarr::Client,
+        endpoint_id: EndpointId,
+        ttl: u32,
+    ) -> Result<(), BevyError> {
+        let delay = Duration::from_secs((ttl as f64 * 0.75) as u64);
+        let endpoint_z32 = endpoint_id.to_z32();
+        let endpoint_name = pkarr::dns::Name::new(&endpoint_z32)?;
+
+        loop {
+            let mut builder = pkarr::SignedPacket::builder();
+            if let Some(most_recent) = client
+                .resolve_most_recent(&self.dns_publisher_keypair().public_key())
+                .await
+            {
+                for record in most_recent.fresh_resource_records(self.bootstrap_dns_name()) {
+                    match record.rdata {
+                        RData::CNAME(ref cname) if cname.0 != endpoint_name => {
+                            match record.ttl.overflowing_sub(most_recent.elapsed()) {
+                                (_, true) => {}
+                                (ttl, false) => {
+                                    let mut record = record.clone();
+                                    record.ttl = ttl;
+                                    builder = builder.record(record);
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            };
+
+            let signed_packet = builder
+                .cname(
+                    self.bootstrap_dns_name().try_into()?,
+                    endpoint_name.clone(),
+                    ttl,
+                )
+                .sign(self.dns_publisher_keypair())?;
+
+            if let Err(e) = client.publish(&signed_packet, None).await {
+                warn!("Failed to publish CNAME: {e:?}");
+                tokio::time::sleep(delay / 3).await;
+            } else {
+                tokio::time::sleep(delay).await;
+            }
+        }
     }
 }
