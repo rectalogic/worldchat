3b94ca6e7c753e0fca75e804e0ed84717d391d4e
Andrew Wason <rectalogic@rectalogic.com>
2026-04-02 12:45:09 -0400

    working irc handshake

--- src/lib.rs
+++ src/lib.rs
@@ -3,44 +3,73 @@ mod chat;
 
 use std::{
     cell::{OnceCell, RefCell},
+    str::FromStr,
     sync::{Arc, Mutex},
 };
 
 pub use app::AppPlugin;
-use bevy::prelude::*;
+use bevy::{prelude::*, tasks::IoTaskPool};
+use futures_util::{SinkExt, StreamExt};
 use wasm_bindgen::prelude::*;
-
-thread_local! {
-    pub static WEBSOCKET: OnceCell<RefCell<(ewebsock::WsSender, ewebsock::WsReceiver)>> = const { OnceCell::new() };
-}
 
 #[wasm_bindgen]
 pub fn start() {
-    let (tx, rx) = ewebsock::connect(
-        "wss://fiery.swiftirc.net:4443",
-        ewebsock::Options {
-            subprotocols: vec!["text.ircv3.net".into()],
-            ..default()
-        },
-    )
-    .unwrap();
-
-    WEBSOCKET.with(|cell| cell.set(RefCell::new((tx, rx))));
-
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Update, move || {
-            WEBSOCKET.with(|cell| {
-                if let Some(ws) = cell.get()
-                    && let (tx, rx) = &mut *ws.borrow_mut()
-                    && let Some(ewebsock::WsEvent::Opened) = rx.try_recv()
-                {
-                    let join =
-                        irc_proto::command::Command::JOIN("#bevyworldchat".into(), None, None);
-                    tx.send(ewebsock::WsMessage::Text((&join).into()));
-                }
-            });
-        })
+        .add_systems(Startup, setup)
         .run();
+
     // App::new().add_plugins(AppPlugin).run();
+}
+
+fn setup() {
+    IoTaskPool::get()
+        .spawn(async move {
+            let ws = tokio_tungstenite_wasm::connect_with_protocols(
+                "wss://fiery.swiftirc.net:4443",
+                &["text.ircv3.net"],
+            )
+            .await
+            .unwrap();
+            let (mut tx, mut rx) = ws.split();
+
+            let user =
+                irc_proto::command::Command::USER("user007".into(), "0".into(), "user007".into());
+            let s = String::from(&user);
+            tx.send(tokio_tungstenite_wasm::Message::text(&s))
+                .await
+                .unwrap();
+
+            let nick = irc_proto::command::Command::NICK("user007".into());
+            let s = String::from(&nick);
+            tx.send(tokio_tungstenite_wasm::Message::text(&s))
+                .await
+                .unwrap();
+
+            while let Some(response) = rx.next().await {
+                info!("{response:?}");
+                if let Ok(tokio_tungstenite_wasm::Message::Text(bytes)) = response
+                    && let irc_proto::command::Command::PING(server1, server2) =
+                        irc_proto::message::Message::from_str(bytes.to_string().as_str())
+                            .unwrap()
+                            .command
+                {
+                    let pong = irc_proto::command::Command::PONG(server1, server2);
+                    let s = String::from(&pong);
+                    tx.send(tokio_tungstenite_wasm::Message::text(&s))
+                        .await
+                        .unwrap();
+                }
+            }
+
+            let join = irc_proto::command::Command::JOIN("#bevyworldchat".into(), None, None);
+            let s = String::from(&join);
+            tx.send(tokio_tungstenite_wasm::Message::text(&s))
+                .await
+                .unwrap();
+            while let Some(response) = rx.next().await {
+                info!("{response:?}");
+            }
+        })
+        .detach();
 }
