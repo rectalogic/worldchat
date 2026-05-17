86bd924e96fa14973dc152a841f25e702b45e727
Andrew Wason <rectalogic@rectalogic.com>
2026-05-17 09:55:22 -0400

    wasm window sizing

--- src/app.rs
+++ src/app.rs
@@ -5,9 +5,21 @@ pub struct AppPlugin;
 
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((DefaultPlugins, IrcServerPlugin, WorldPlugin))
-            .init_state::<AppState>()
-            .set_error_handler(bevy::ecs::error::error);
+        app.add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "WorldChat".into(),
+                    canvas: Some("#canvas".into()),
+                    fit_canvas_to_parent: true,
+                    ..default()
+                }),
+                ..default()
+            }),
+            IrcServerPlugin,
+            WorldPlugin,
+        ))
+        .init_state::<AppState>()
+        .set_error_handler(bevy::ecs::error::error);
     }
 }
 
